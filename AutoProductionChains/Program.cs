0d6e553b52c6f6824cd6a6df465b313fbfbdf68d
PARRYH0TTEr <81820845+PARRYH0TTEr@users.noreply.github.com>
2024-03-07 23:35:54 +0100

    first commit

---
+++ AutoProductionChains/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AutoProductionChains
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
